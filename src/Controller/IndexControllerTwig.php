ba61e41df23fad4473eac43bfe3a487b903b5f2a
Sandy Nguyen <sang22@student.bth.se>
2024-04-02 17:39:49 +0200

    Webb requirements

---
+++ src/Controller/IndexControllerTwig.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexControllerTwig extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function number(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+    
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+    #[Route("/lucky", name: "lucky")]
+    public function lucky(): Response
+    {
+        return $this->render('lucky.html.twig');
+    }
+}
