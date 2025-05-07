8c9eea6465626f8b8159fbb67fabcf031c1dbc43
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-05-07 12:19:15 +0200

    Added Game structure

---
+++ src/Controller/GameController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+class GameController extends AbstractController
+{
+    #[Route("/game", name: "game_start")]
+    public function home(): Response
+    {
+        return $this->render('game/home.html.twig');
+    }
+    #[Route("/game/doc", name: "game_doc")]
+    public function doc(): Response
+    {
+        return $this->render('game/doc.html.twig');
+    }
+
+}
